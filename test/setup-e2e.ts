e39e592546d2b54cab784e409ddf36983ecc76e9
luc4sGabriel <lucas.g4briel.04@gmail.com>
2025-11-10 12:47:38 -0300

    test: isolated database environment for e2e tests

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,1 +1,35 @@
-console.log('oi');
+import { execSync } from 'child_process'
+import { randomUUID } from 'crypto'
+import 'dotenv/config'
+
+import { PrismaClient } from 'generated/prisma'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseUrl(schemaId: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL environment variable is not set.')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schemaId)
+
+    return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+    const databaseUrl = generateUniqueDatabaseUrl(schemaId)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+
+    await prisma.$disconnect()
+})
